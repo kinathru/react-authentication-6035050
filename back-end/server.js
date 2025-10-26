6f875678f021601b9d9821f7ae11f1071ff8286f
kinath <kinath.ru@gmail.com>
2025-10-26 17:24:56 -0500

    feat:implemented the sign up route

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,46 @@
 const express = require('express');
+const {db, saveDb} = require('./db');
+const bcrypt = require('bcrypt');
+const {v4: uuidv4} = require('uuid');
+
 
 const app = express();
 app.use(express.json());
 
 // Endpoints go here
+
+// Sign up endpoint
+app.post('/api/sign-up', async (req, res) => {
+    const {email, password} = req.body;
+
+    // Make sure there's no user with the email already in the database
+    const matching_user = db.users.find(user => user.email === email);
+    if (matching_user) {
+        return res.sendStatus(409);
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const id = uuidv4();
+
+    const startingInfo = {
+        hairColor: "",
+        favoriteFood: "",
+        bio: "",
+    }
+
+    db.users.push({
+        id,
+        email,
+        passwordHash,
+        info: startingInfo,
+        isVerified: false
+    });
+
+    saveDb();
+
+    res.json({id});
+
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
