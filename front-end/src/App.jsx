95f08e5afa84b81b3f01748ad8e548c3910869d8
kinath <kinath.ru@gmail.com>
2025-10-26 10:24:35 -0500

    feat:implemented private route component to redirect to a page when not allowed

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router";
-import { SignUpPage } from './SignUpPage';
-import { LogInPage } from './LogInPage';
-import { UserInfoPage } from './UserInfoPage';
+import {BrowserRouter, Routes, Route} from "react-router";
+import {SignUpPage} from './SignUpPage';
+import {LogInPage} from './LogInPage';
+import {UserInfoPage} from './UserInfoPage';
+import {PrivateRoute} from "./PrivateRoute.jsx";
 
 function App() {
-  return (
-    <div className="page-container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/log-in" element={<LogInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/" element={<UserInfoPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div className="page-container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/log-in" element={<LogInPage/>}/>
+                    <Route path="/sign-up" element={<SignUpPage/>}/>
+
+                    {/*Purpose of the private route is to redirect the user to the log-in page when the user tries to
+                    access '/' path if the user is not authenticated*/}
+                    <Route element={<PrivateRoute redirectPath="log-in" isAllowed={true}/>}>
+                        <Route path="/" element={<UserInfoPage/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
