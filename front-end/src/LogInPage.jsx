a164fad8e2d926f66c5b8a4ac18a0ad8e22d969f
kinath <kinath.ru@gmail.com>
2025-10-27 20:28:40 -0500

    feat:implemented basic login and signup functionalities

--- front-end/src/LogInPage.jsx
+++ front-end/src/LogInPage.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useToken} from "./useToken.js";
+import axios from 'axios';
 
 export const LogInPage = () => {
-  const [errorMessage, setErrorMessage] = useState('');
+    const [token, setToken] = useToken();
+    const [errorMessage, setErrorMessage] = useState('');
 
-  const [emailValue, setEmailValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
 
-  // We'll use the history to navigate the user
-  // programmatically later on (we're not using it yet)
-  const navigate = useNavigate();
+    // We'll use the history to navigate the user
+    // programmatically later on (we're not using it yet)
+    const navigate = useNavigate();
 
-  const onLogInClicked = async () => {
-    alert('Log in not implemented yet');
-  }
+    const onLogInClicked = async () => {
+        const response = await axios.post('/api/log-in', {
+            email: emailValue,
+            password: passwordValue
+        });
+        const token = response.data.token;
+        setToken(token);
+        navigate('/');
+    }
 
-  return (
-    <div className="content-container">
-      <h1>Log In</h1>
-      {errorMessage && <div className="fail">{errorMessage}</div>}
-      <input
-        value={emailValue}
-        onChange={e => setEmailValue(e.target.value)}
-        placeholder="someone@gmail.com" />
-      <input
-        type="password"
-        value={passwordValue}
-        onChange={e => setPasswordValue(e.target.value)}
-        placeholder="password" />
-      <hr />
-      <button
-        disabled={!emailValue || !passwordValue}
-        onClick={onLogInClicked}>Log In</button>
-      <button onClick={() => navigate('/forgot-password')}>Forgot your password?</button>
-      <button onClick={() => navigate('/sign-up')}>Don't have an account? Sign Up</button>
-    </div>
-  );
+    return (
+        <div className="content-container">
+            <h1>Log In</h1>
+            {errorMessage && <div className="fail">{errorMessage}</div>}
+            <input
+                value={emailValue}
+                onChange={e => setEmailValue(e.target.value)}
+                placeholder="someone@gmail.com"/>
+            <input
+                type="password"
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                placeholder="password"/>
+            <hr/>
+            <button
+                disabled={!emailValue || !passwordValue}
+                onClick={onLogInClicked}>Log In
+            </button>
+            <button onClick={() => navigate('/forgot-password')}>Forgot your password?</button>
+            <button onClick={() => navigate('/sign-up')}>Don't have an account? Sign Up</button>
+        </div>
+    );
 }
--- front-end/src/SignUpPage.jsx
+++ front-end/src/SignUpPage.jsx
@@ -1,45 +1,55 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useToken} from "./useToken.js";
+import axios from 'axios';
 
 export const SignUpPage = () => {
-  const [errorMessage, setErrorMessage] = useState('');
+    const [token, setToken] = useToken();
+    const [errorMessage, setErrorMessage] = useState('');
 
-  const [emailValue, setEmailValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-  const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const onSignUpClicked = async () => {
-    alert('Sign up not implemented yet');
-  }
+    const onSignUpClicked = async () => {
+        const response = await axios.post('/api/sign-up', {
+            email: emailValue,
+            password: passwordValue
+        });
+        const token = response.data.token;
+        setToken(token);
+        navigate('/');
+    }
 
-  return (
-    <div className="content-container">
-      <h1>Sign Up</h1>
-      {errorMessage && <div className="fail">{errorMessage}</div>}
-      <input
-        value={emailValue}
-        onChange={e => setEmailValue(e.target.value)}
-        placeholder="someone@gmail.com" />
-      <input
-        type="password"
-        value={passwordValue}
-        onChange={e => setPasswordValue(e.target.value)}
-        placeholder="password" />
-      <input
-        type="password"
-        value={confirmPasswordValue}
-        onChange={e => setConfirmPasswordValue(e.target.value)}
-        placeholder="password" />
-      <hr />
-      <button
-        disabled={
-          !emailValue || !passwordValue ||
-          passwordValue !== confirmPasswordValue
-        }
-        onClick={onSignUpClicked}>Sign Up</button>
-      <button onClick={() => navigate('/log-in')}>Already have an account? Log In</button>
-    </div>
-  );
+    return (
+        <div className="content-container">
+            <h1>Sign Up</h1>
+            {errorMessage && <div className="fail">{errorMessage}</div>}
+            <input
+                value={emailValue}
+                onChange={e => setEmailValue(e.target.value)}
+                placeholder="someone@gmail.com"/>
+            <input
+                type="password"
+                value={passwordValue}
+                onChange={e => setPasswordValue(e.target.value)}
+                placeholder="password"/>
+            <input
+                type="password"
+                value={confirmPasswordValue}
+                onChange={e => setConfirmPasswordValue(e.target.value)}
+                placeholder="password"/>
+            <hr/>
+            <button
+                disabled={
+                    !emailValue || !passwordValue ||
+                    passwordValue !== confirmPasswordValue
+                }
+                onClick={onSignUpClicked}>Sign Up
+            </button>
+            <button onClick={() => navigate('/log-in')}>Already have an account? Log In</button>
+        </div>
+    );
 }
