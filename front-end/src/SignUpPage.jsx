e4cab1ac66f655788a6a8bb851296f264b3b8ad2
kinath <kinath.ru@gmail.com>
2025-11-01 12:38:05 -0500

    fix:fix redirection after user signing up

--- front-end/src/SignUpPage.jsx
+++ front-end/src/SignUpPage.jsx
@@ -20,7 +20,7 @@ export const SignUpPage = () => {
         });
         const token = response.data.token;
         setToken(token);
-        navigate('/');
+        navigate('/', { replace: true });
     }
 
     return (
